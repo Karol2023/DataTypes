accd4a4e850993a665e5c284ad30afbc360100a2
Karol <karolzymkowski1990@gmail.com>
2024-02-21 05:00:40 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,33 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        char zmChar = 'B';
+        System.out.println(zmChar);
+        //String
+        String zmString = "Bartek";
+        System.out.println(zmString);
+        //Boolean
+        boolean zmBollean = true;
+        System.out.println(zmBollean);
+        //byte
+        byte zmByte = Byte.MIN_VALUE;
+        byte zmByte2 = Byte.MAX_VALUE;
+        System.out.println("Byte: " + zmByte + " - " + zmByte2);
+        //shot
+        short zmShort = Short.MAX_VALUE;
+        short zmShrt2 = Short.MAX_VALUE;
+        System.out.println("Short: " + zmShort + " - " + zmShrt2);
+        //int
+        int zmInt = Integer.MIN_VALUE;
+        int zmInt2 = Integer.MAX_VALUE;
+        System.out.println("Int: " + zmInt + " - " + zmInt2);
+        //long
+        long zmLong = Long.MIN_VALUE;
+        long zmLong2 = Long.MAX_VALUE;
+        System.out.println("Long: " + zmLong + " - " + zmLong2);
+        //float
+        float zmFloat = Float.MIN_VALUE;
+        float zmFloat2 = Float.MAX_VALUE;
+        System.out.println("Float: " + zmFloat + " - " + zmFloat2);
         }
     }
-}
